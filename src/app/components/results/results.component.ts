ceedf1f47fef7305a920baf5776269dad91784bd
kabaniak <kabaniak@gmail.com>
2020-10-04 03:37:06 -0400

    working on displaying results

--- src/app/components/results/results.component.ts
+++ src/app/components/results/results.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireStorage } from "@angular/fire/storage";
+import { AngularFirestore } from "@angular/fire/firestore";
 
 @Component({
   selector: 'app-results',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private storage: AngularFireStorage, private db: AngularFirestore) {
+   }
 
   ngOnInit() {
+
+    const dbRef = this.db.collection('userPostings');
+
+    const searchQuery = this.db.collection('userPostings', ref => ref.where('name', '==', 'Katrina')); //go back and make better later
+
+    const snapshot = searchQuery.get();
+    if (snapshot == null) {
+      console.log('No matching documents.');
+      return;
+    }  
+
+    snapshot.forEach(doc => {
+      console.log(doc[1]);
+    });
+
   }
 
 }
